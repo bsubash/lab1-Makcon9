28e0e5ed433884ad45ec3f2950a2fd3396549509
Makcon9 <01tankist1999@mail.ru>
2019-03-12 12:04:06 +0300

    second task of the first lab

---
+++ lab01/lab01_z13.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "13.Сравнить две числовые переменные и если первая больше, то вывести сообщение об этом, используя команду test
+"   
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+echo "enter first number"
+read num1
+echo "enter second number"
+read num2
+if test $num1 \> $num2
+then
+echo "first number is larger than the second number"
+elif test $num1 = $num2
+then
+	echo "numbers are equal"
+else 
+	echo "second number is laregr"
+fi
