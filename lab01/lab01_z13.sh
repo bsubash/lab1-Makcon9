8af82560f0f9ed6abc6a695e09ab845393024348
Makcon9 <01tankist1999@mail.ru>
2019-04-02 12:37:49 +0300

    corrected

--- lab01/lab01_z13.sh
+++ lab01/lab01_z13.sh
@@ -18,10 +18,10 @@ echo "enter first number"
 read num1
 echo "enter second number"
 read num2
-if test $num1 \> $num2
+if test $num1 -gt $num2
 then
 echo "first number is larger than the second number"
-elif test $num1 = $num2
+elif test $num1 -eq $num2
 then
 	echo "numbers are equal"
 else 
